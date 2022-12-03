0298875796961d2ed76996767eeb1bd9bd51fe74
Luis Henrique <luique16@outlook.com>
2022-12-02 22:48:26 -0400

    primeira etapa js feita

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,32 @@
 import '../css/style.css'
+
+
+const input_pesquisa = document.querySelector('#input_pesquisa');
+const btn_localizar = document.querySelector('#btn_localizar');
+const display_pokedex = document.querySelector('#display');
+
+
+btn_localizar.addEventListener('click', async () => {
+
+    const pokemon_dados = await localizar_pokemon(input_pesquisa.value);
+
+    create_card(pokemon_dados);
+})
+
+async function localizar_pokemon(pokemon_id) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon_id}`
+    const response = await fetch(url);
+    const pokemon = await response.json()
+    console.log(pokemon);
+    return pokemon;
+}
+
+function create_card(pokemon) {
+    const card_pokemon = document.createElement('div')
+    card_pokemon.className = 'cartao_pokemon';
+    card_pokemon.innerHTML = `
+    <img class="pokemon_sprite" src="${pokemon.sprites.front_default}" >
+    <h2>${pokemon.name}</h2>
+ `
+ display_pokedex.appendChild(card_pokemon);
+}
