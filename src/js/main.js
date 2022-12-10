95470f5383d269ff494c20f9fe214fa29df6a651
Luis Henrique <luique16@outlook.com>
2022-12-09 22:34:03 -0400

    estilizacao

--- src/js/main.js
+++ src/js/main.js
@@ -23,10 +23,12 @@ async function localizar_pokemon(pokemon_id) {
 
 function create_card(pokemon) {
     const card_pokemon = document.createElement('div')
-    card_pokemon.className = 'cartao_pokemon';
+    card_pokemon.className = `cartao_pokemon ${pokemon.types[0].type.name}_border`;
     card_pokemon.innerHTML = `
-    <img class="pokemon_sprite" src="${pokemon.sprites.front_default}" >
-    <h2>${pokemon.name}</h2>
+    <div class="topo_card  ${pokemon.types[0].type.name}"><h2>${pokemon.name}</h2></div>
+    <img class="pokemon_sprite" src="${pokemon.sprites.other.dream_world.front_default}" >
+    
  `
+ display_pokedex.innerHTML = '';
  display_pokedex.appendChild(card_pokemon);
 }
