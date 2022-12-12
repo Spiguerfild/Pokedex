0cb67ddd9e074dc761fab87682e0cf89e90b0b1e
Luis Henrique <luique16@outlook.com>
2022-12-12 17:15:38 -0400

    , arrumando codigo

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,38 @@
-import '../css/style.css'
+import "../css/style.css";
 
+const input_pesquisa = document.querySelector("#input_pesquisa");
+const btn_localizar = document.querySelector("#btn_localizar");
+const display_pokedex = document.querySelector("#display");
 
-const input_pesquisa = document.querySelector('#input_pesquisa');
-const btn_localizar = document.querySelector('#btn_localizar');
-const display_pokedex = document.querySelector('#display');
+btn_localizar.addEventListener("click", async () => {
+  const pokemon_dados = await localizar_pokemon(input_pesquisa.value);
 
-
-btn_localizar.addEventListener('click', async () => {
-
-    const pokemon_dados = await localizar_pokemon(input_pesquisa.value);
-
-    create_card(pokemon_dados);
-})
+  create_card(pokemon_dados);
+});
 
 async function localizar_pokemon(pokemon_id) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon_id}`
-    const response = await fetch(url);
-    const pokemon = await response.json()
-    console.log(pokemon);
-    return pokemon;
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemon_id}`;
+  const response = await fetch(url);
+  const pokemon = await response.json();
+  console.log(pokemon);
+  return pokemon;
 }
 
 function create_card(pokemon) {
-    const card_pokemon = document.createElement('div')
-    card_pokemon.className = `cartao_pokemon ${pokemon.types[0].type.name}_border`;
-    card_pokemon.innerHTML = `
+  const card_pokemon = document.createElement("div");
+  card_pokemon.className = `cartao_pokemon ${pokemon.types[0].type.name}_border`;
+  card_pokemon.innerHTML = `
     <div class="topo_card  ${pokemon.types[0].type.name}"><h2>${pokemon.name}</h2></div>
     <img class="pokemon_sprite" src="${pokemon.sprites.other.dream_world.front_default}" >
-    
- `
- display_pokedex.innerHTML = '';
- display_pokedex.appendChild(card_pokemon);
+    <div class="info">Status${pokemon.stats[1].base_stat}
+    <div class="esquerda_card">
+    <div>Status base: </div>
+    <div>Type: </div>
+    </div>
+
+    <div class="direita"></div>
+    </div>
+ `;
+  display_pokedex.innerHTML = "";
+  display_pokedex.appendChild(card_pokemon);
 }
